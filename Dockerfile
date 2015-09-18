2353222648a8e9c4d7990972ed8ee54743879dbc
seti <seti@setadesign.net>
2015-09-18 17:48:51 +0200

    Some fixes cause of loosing settings after recreate a container even if using persistent volume

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ MAINTAINER Seti <seti@setadesign.net>
 
 ENV APP_VERSION 1.0
 ENV APP_BUILD $APP_VERSION.583
-ENV APP_HOME /var/lib/hub
+ENV APP_HOME /data
 
 
 RUN curl -L https://download.jetbrains.com/hub/$APP_VERSION/hub-ring-bundle-$APP_BUILD.zip -o /opt/hub.zip && \
@@ -21,6 +21,6 @@ WORKDIR /opt/hub
 ADD run.sh /opt/hub/
 RUN chmod o+rx run.sh
 USER hub
-ENTRYPOINT ["run.sh"]
+ENTRYPOINT ["/opt/hub/run.sh"]
 EXPOSE 8080
 VOLUME ["$APP_HOME"]
--- run.sh
+++ run.sh
@@ -3,14 +3,18 @@
 if [ ! -e $APP_HOME/conf ];
 then
 	echo "first run: starting configure"
-	ln -s $APP_DIR/conf $APP_HOME/conf
+	mv /opt/hub/conf $APP_HOME/
+	ln -s $APP_HOME/conf /opt/hub/conf
 	bin/hub.sh configure \
 		--backups-dir $APP_HOME/backups \
 		--data-dir    $APP_HOME/data \
 		--logs-dir    $APP_HOME/log \
 		--temp-dir    $APP_HOME/tmp \
-		--listen-port $APP_PORT \
+		--listen-port 8080 \
 		--base-url    http://localhost/
+else
+	rm -rf /opt/hub/conf
+	ln -s $APP_HOME/conf /opt/hub/conf
 fi
 
 bin/hub.sh run
